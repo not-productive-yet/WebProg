290f81f641f6af3d9c83bf32502878808c74e870
not-productive-yet <lisa.trovato1@yahoo.com>
2019-10-11 15:00:16 +0200

    grundgerüst

---
+++ src/logik/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+import stylesheet from "../styling/app.css";
+
+/**
+ * Hauptklasse der Anwendung. Kümmert sich darum, die Anwendung auszuführen
+ * und die angeforderten Bildschirmseiten anzuzeigen.
+ */
+export default class App {
+  constructor() {}
+
+  /**
+   * Ab hier beginnt die Anwendung zu laufen.
+   */
+  start() {
+    console.log("Die Klasse App sagt Hallo!");
+  }
+}
