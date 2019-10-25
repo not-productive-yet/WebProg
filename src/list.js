371777c9bd3a499446a86748c7c96610e87a647a
Jannik Steck <jannik.steck@gmail.com>
2019-10-25 14:07:05 +0200

    Sollte jetzt wieder passen

--- src/list.js
+++ src/list.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-var elements = [{ fach: "webprog", todo: "app erstellen", tageBisFällig: 5 }];
-
-function load() {
-  var html = "<ul>";
-=======
 var elements = [
   { fach: "webprog", todo: "app erstellen", tageBisFällig: 5 },
   { fach: "mobile", todo: "testen", tageBisFällig: 6 }
@@ -25,19 +19,10 @@ window.onload = function show() {
 
 function load() {
   var html = "<ul class='entries'>";
->>>>>>> 249574e2f9fd2259fe1ee9fefa6a9a5d01c47980
   for (var i = 0; i < elements.length; i++) {
     html +=
       "<li>" +
       elements[i].fach +
-<<<<<<< HEAD
-      elements[i].todo +
-      " " +
-      elements[i].tageBisFällig +
-      '<button class="remove" id="' +
-      i +
-      '">x</button></li>';
-=======
       " " +
       elements[i].todo +
       " " +
@@ -49,23 +34,14 @@ function load() {
       '<button class="edit" id="' +
       i +
       '">edit</button></li>';
->>>>>>> 249574e2f9fd2259fe1ee9fefa6a9a5d01c47980
   }
   html += "</ul>";
 
   document.getElementById("list").innerHTML = html;
 }
 
-<<<<<<< HEAD
-window.onload = function show() {
-  load();
-};
-
-function add() {
-=======
 function add() {
   //muss wahrsch async sein damit er auf user input wartet
->>>>>>> 249574e2f9fd2259fe1ee9fefa6a9a5d01c47980
   var newFach = document.getElementById("inputFach").value;
   var newTodo = document.getElementById("inputTodo").value;
   var newTage = document.getElementById("inputTage").value;
@@ -74,15 +50,6 @@ function add() {
     todo: newTodo,
     tageBisFällig: newTage
   });
-<<<<<<< HEAD
-  console.log(elements);
-  load();
-}
-
-document.getElementById("plusButton").addEventListener("click", add());
-
-function remove() {}
-=======
   hide();
   load();
 }
@@ -103,4 +70,3 @@ function display() {
 function hide() {
   addEntry.className = "hide";
 }
->>>>>>> 249574e2f9fd2259fe1ee9fefa6a9a5d01c47980
