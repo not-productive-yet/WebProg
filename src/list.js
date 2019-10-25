249574e2f9fd2259fe1ee9fefa6a9a5d01c47980
not-productive-yet <lisa.trovato1@yahoo.com>
2019-10-25 11:55:14 +0200

    js anbindung funzt, css und js erweitert, bug bei remove und hide

--- src/list.js
+++ src/list.js
@@ -1,28 +1,47 @@
-var elements = [{ fach: "webprog", todo: "app erstellen", tageBisFällig: 5 }];
+var elements = [
+  { fach: "webprog", todo: "app erstellen", tageBisFällig: 5 },
+  { fach: "mobile", todo: "testen", tageBisFällig: 6 }
+];
+
+window.onload = function show() {
+  load();
+  var addEntry = document.getElementById("addEntry");
+  addEntry.addEventListener("click", add);
+  var classname = document.getElementsByClassName("remove");
+  for (var i = 0; i < classname.length; i++) {
+    classname[i].addEventListener("click", remove);
+  }
+  var goToAddEntry = document.getElementById("goToAddEntry");
+  goToAddEntry.addEventListener("click", display);
+
+  var addEntry = document.getElementsByClassName("addEntry");
+};
 
 function load() {
-  var html = "<ul>";
+  var html = "<ul class='entries'>";
   for (var i = 0; i < elements.length; i++) {
     html +=
       "<li>" +
       elements[i].fach +
+      " " +
       elements[i].todo +
       " " +
       elements[i].tageBisFällig +
+      " " +
       '<button class="remove" id="' +
       i +
-      '">x</button></li>';
+      '">x</button></li>' +
+      '<button class="edit" id="' +
+      i +
+      '">edit</button></li>';
   }
   html += "</ul>";
 
   document.getElementById("list").innerHTML = html;
 }
 
-window.onload = function show() {
-  load();
-};
-
 function add() {
+  //muss wahrsch async sein damit er auf user input wartet
   var newFach = document.getElementById("inputFach").value;
   var newTodo = document.getElementById("inputTodo").value;
   var newTage = document.getElementById("inputTage").value;
@@ -31,10 +50,23 @@ function add() {
     todo: newTodo,
     tageBisFällig: newTage
   });
-  console.log(elements);
+  hide();
   load();
 }
 
-document.getElementById("plusButton").addEventListener("click", add());
+function remove() {
+  console.log("remove aufgerufen");
+  var id = this.getAttribute("id");
+  elements.splice(id, 1);
+  load();
+}
 
-function remove() {}
+function edit() {}
+
+function display() {
+  addEntry.className = "show";
+}
+
+function hide() {
+  addEntry.className = "hide";
+}
