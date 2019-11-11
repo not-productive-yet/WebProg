8f265a531a722b53202994e6a9129d53c74df939
not-productive-yet <lisa.trovato1@yahoo.com>
2019-11-11 13:45:49 +0100

    merge marker entfernt

--- src/fächer/fächerList.js
+++ src/fächer/fächerList.js
@@ -6,7 +6,6 @@ export async function loadFächer() {
 
   console.log(elements);
   var html = "<ul class='entries' id='fächerEntries'>";
-<<<<<<< HEAD
   if (elements.length == 0) {
     html += "<p id='leer'> NOCH KEINE AUFGABEN VORHANDEN </p>";
   } else {
@@ -44,44 +43,6 @@ export async function loadFächer() {
         "<div>" +
         "</li>";
     }
-=======
-  for (var i = 0; i < elements.length; i++) {
-    html +=
-      "<li id='kastenAußen'>" +
-      "<div id='kastenInnen'>" +
-      "<div id ='fachD'>"+
-      elements[i].fachD +
-      "</div>"+
-      "<div id='dozent'>" +
-      "Dozent: " +
-      elements[i].dozent +
-      "</div>" +
-      " " +
-      "<div id='email'>"+
-      " E-Mail: " +
-      elements[i].email +
-      "</div>" +
-      " " +
-      "<div id='klausurDatum'>" +
-      "Klausur-Termin: " +
-      elements[i].klausurDatum +
-      "</div>" +
-      " " +
-      "<div id='notizD'>" +
-      "Notiz: " +
-      elements[i].notizD +
-      "</div>" +
-      " " +
-      "<div id='buttonFächer'>"+
-      '<a href="/neuesFach" data-navigo><button type="button" class="saveAndBack" class="button" class="editFach" id="' +
-      i +
-      '">Edit-Fach</button></a>' +
-
-      "</div>"+
-      "</div>"+
-      "</li>";
-  }
->>>>>>> e1658242a12d3cad6cdc0a2a55546c1b6c63108a
 
     html += "</ul>";
   }
