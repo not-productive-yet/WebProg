5ce0286c2d311d5424e98e53dc5e21d5d5c14faa
ReneH-DHBW <56265360+ReneH-DHBW@users.noreply.github.com>
2019-11-15 14:44:43 +0100

    Hintergrund ändern geht jetzt; Pfade noch zu klären

--- src/designs/designs.js
+++ src/designs/designs.js
@@ -1,13 +1,14 @@
 "use strict";
-
 import stylesheet from "./designs.css";
-
 export default class Designs {
   constructor(app) {
     this._app = app;
   }
   onShow() {
     let section = document.querySelector("#designs").cloneNode(true);
+    section.querySelector('#dunkel').addEventListener("click", aufDunkel);
+    section.querySelector('#hell').addEventListener("click", aufHell);
+
 
     return {
       className: "designs",
@@ -23,3 +24,12 @@ export default class Designs {
     return "Designs";
   }
 }
+export async function aufDunkel() {
+      alert("Button geht");
+      document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2600&q=80)";
+    }
+export async function aufHell() {
+      alert("Button geht auch");
+      document.body.style.backgroundImage = "url('./images/journeyNav.png')";
+
+    }
