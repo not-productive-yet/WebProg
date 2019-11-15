dcffde3673d19566ac88a68dbb0f398cf1e4f516
ReneH-DHBW <56265360+ReneH-DHBW@users.noreply.github.com>
2019-11-15 20:50:18 +0100

    Design weiter bearbeitet, Farben müssenw wir noch anpassen

--- src/designs/designs.js
+++ src/designs/designs.js
@@ -8,6 +8,7 @@ export default class Designs {
     let section = document.querySelector("#designs").cloneNode(true);
     section.querySelector('#dunkel').addEventListener("click", aufDunkel);
     section.querySelector('#hell').addEventListener("click", aufHell);
+    section.querySelector('#normal').addEventListener("click", aufNormal);
 
 
     return {
@@ -24,12 +25,26 @@ export default class Designs {
     return "Designs";
   }
 }
-export async function aufDunkel() {
-      alert("Button geht");
+export async function aufHell() {
       document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2600&q=80)";
     }
-export async function aufHell() {
-      alert("Button geht auch");
-      document.body.style.backgroundImage = "url('./images/journeyNav.png')";
-
+export async function aufDunkel() {
+    document.body.style.setProperty("--dark-green-color", "red");
+    document.body.style.setProperty("--dark-green-transparent-color", "red");
+    document.body.style.setProperty("--dark-green-less-transparent-color", "red");
+    document.body.style.setProperty("--light-green-hover-color", "red");
+    document.body.style.setProperty("--nav-selected-color", "red");
+    document.body.style.setProperty("--fächer-green-color", "red");
+    document.body.style.setProperty("--fächer-dark-green-color", "red");
+    document.body.style.setProperty("--schrift-color", "rgba(0,0,0)");
+    }
+export async function aufNormal() {
+    document.body.style.setProperty("--dark-green-color", "rgb(55, 74, 69, 0.8)");
+    document.body.style.setProperty("--dark-green-transparent-color", "rgba(121, 148, 141, 0.7)");
+    document.body.style.setProperty("--dark-green-less-transparent-color", "rgba(121, 148, 141, 0.9)");
+    document.body.style.setProperty("--light-green-hover-color", "rgb(212, 255, 235)");
+    document.body.style.setProperty("--nav-selected-color", "rgba(119, 149, 145)");
+    document.body.style.setProperty("--fächer-green-color", "rgb(94, 123, 119, 0.7)");
+    document.body.style.setProperty("--fächer-dark-green-color", "rgba(70, 98, 95)");
+    document.body.style.setProperty("--schrift-color", "rgba(255,255,255)");
     }
