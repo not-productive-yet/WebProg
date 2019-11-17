0fc95a6941e7b9d8a24e1c7df58ed659d8de0a1b
ReneH-DHBW <56265360+ReneH-DHBW@users.noreply.github.com>
2019-11-17 12:36:07 +0100

    Bisschen aufgeräumt

--- src/designs/designs.js
+++ src/designs/designs.js
@@ -6,9 +6,9 @@ export default class Designs {
   }
   onShow() {
     let section = document.querySelector("#designs").cloneNode(true);
-    section.querySelector('#dunkel').addEventListener("click", aufDunkel);
-    section.querySelector('#hell').addEventListener("click", aufHell);
-    section.querySelector('#normal').addEventListener("click", aufNormal);
+    section.querySelector('#dunkel').addEventListener("click", rot);
+    section.querySelector('#hell').addEventListener("click", gelb);
+    section.querySelector('#normal').addEventListener("click", braun);
 
 
     return {
@@ -25,21 +25,7 @@ export default class Designs {
     return "Designs";
   }
 }
-async function aufHell() {
-  document.body.style.backgroundImage = "url(https://images.alphacoders.com/294/thumb-1920-294151.gif)";
-  document.body.style.setProperty("--dark-green-color", " rgb(52, 60, 69, 0.8");
-  document.body.style.setProperty("--dark-green-transparent-color", " rgba(119, 127, 146, 0.7");
-  document.body.style.setProperty("--dark-green-transparent-color-hover", " rgb(119, 127, 146");
-  document.body.style.setProperty("--dark-green-transparent-color-hover-medium", " rgb(147, 147, 173");
-  document.body.style.setProperty("--dark-green-transparent-color-hover-light", " rgb(173, 172, 199");
-  document.body.style.setProperty("--dark-green-less-transparent-color", " rgba(119, 123, 146, 0.9");
-  document.body.style.setProperty("--light-green-hover-color", " rgb(210, 209, 255");
-  document.body.style.setProperty("--nav-selected-color", " rgba(117, 121, 147");
-  document.body.style.setProperty("--fächer-green-color", " rgb(93, 96, 119, 0.7");
-  document.body.style.setProperty("--fächer-dark-green-color", " rgba(68, 76, 94");
-  document.body.style.setProperty("--schrift-color", " rgba(255, 255, 255");
-    }
-    async function aufDunkel() {
+async function rot() {
   document.body.style.backgroundImage = "url(https://images5.alphacoders.com/711/thumb-1920-711435.jpg)";
   document.body.style.setProperty("--dark-green-color", "rgb(141, 62, 62, 0.8");
   document.body.style.setProperty("--dark-green-transparent-color", " rgba(172, 108, 108, 0.7");
@@ -53,7 +39,22 @@ async function aufHell() {
   document.body.style.setProperty("--fächer-dark-green-color", " rgba(94, 68, 68");
   document.body.style.setProperty("--schrift-color", " rgba(255, 255, 255");
     }
-async function aufNormal() {
+async function gelb() {
+  document.body.style.backgroundImage = "url(https://images.alphacoders.com/294/thumb-1920-294151.gif)";
+  document.body.style.setProperty("--dark-green-color", " rgb(52, 60, 69, 0.8");
+  document.body.style.setProperty("--dark-green-transparent-color", " rgba(119, 127, 146, 0.7");
+  document.body.style.setProperty("--dark-green-transparent-color-hover", " rgb(119, 127, 146");
+  document.body.style.setProperty("--dark-green-transparent-color-hover-medium", " rgb(147, 147, 173");
+  document.body.style.setProperty("--dark-green-transparent-color-hover-light", " rgb(173, 172, 199");
+  document.body.style.setProperty("--dark-green-less-transparent-color", " rgba(119, 123, 146, 0.9");
+  document.body.style.setProperty("--light-green-hover-color", " rgb(210, 209, 255");
+  document.body.style.setProperty("--nav-selected-color", " rgba(117, 121, 147");
+  document.body.style.setProperty("--fächer-green-color", " rgb(93, 96, 119, 0.7");
+  document.body.style.setProperty("--fächer-dark-green-color", " rgba(68, 76, 94");
+  document.body.style.setProperty("--schrift-color", " rgba(255, 255, 255");
+    }
+
+async function braun() {
   document.body.style.backgroundImage = "url(https://i.pinimg.com/originals/35/0b/f3/350bf3ecb77455ac53a9651f2425bc09.jpg)";
   document.body.style.setProperty("--dark-green-color", " rgb(55, 74, 69, 0.8");
   document.body.style.setProperty("--dark-green-transparent-color", " rgba(121, 148, 141, 0.7");
